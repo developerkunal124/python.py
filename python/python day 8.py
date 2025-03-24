d0125ecc502d0ef3e8931cfc2e21d2cb815230d5
developerkunal124 <kunalraghav153@gmail.com>
2025-03-24 11:11:02 +0530

    for loop changes.py

---
+++ python/python day 8.py
@@ -0,0 +1,14 @@
+# for loop in python.
+#name ="kunal"
+#or :
+# i in name:  # the iterate of kunal in sequence........>>>>>>>>
+  #  print(i)
+
+for k in range(5):
+   # print(k)>>>>>>>>>> .. that this is the range function..<<<<<<<<<<<<<
+    print(k+ 1)
+
+   # for k in range(1, 200):
+    #    print(k)
+    for k in range(4, 12, 3):
+        print(k)
