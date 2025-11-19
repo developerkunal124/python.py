b2d4e8b8c6fbe5f40107a7932ff536a79d0ef16a
developerkunal124 <kunalraghav153@gmail.com>
2025-11-19 21:03:44 +0530

    star pattern

--- python/python day 9.py
+++ python/python day 9.py
@@ -1,5 +1,50 @@
-#while loop in python...>>>>>>>>>>
+#while loop in python...>>>>
+while True:
+    print("welcometo calculator")
+    print("1:for addition")
+    print("2:for subtration")
+    print("3:for multiple")
+    print("4:for divide")
+    print("5:exit")
 
-for k in range(5):
-    print(k)
-    
+    choose= input("select value ()")
+
+        
+        
+
+
+  
+
+    if choose =='5':
+        print("exiting the loop sir!")
+        break   
+
+    a = int(input("enter the value"))
+    b = int(input("enter the seconr value"))
+
+    if choose == '1':
+        print(f"adding {a} + {b} = {a+b}")
+
+    elif choose =='2':
+        print(f"subtraction {a} - {b} = {a-b}")
+
+    elif choose =='3':
+        print(f"multiplication {a} + {b} = {ab}")
+
+
+    elif choose =='4':
+        print(f"divide {a} / {b} = {a/b}")
+        
+    else:
+        print("exit loop")
+         ## string in python the  pyhtonyb 
+
+         
+
+
+        
+
+         
+
+
+
